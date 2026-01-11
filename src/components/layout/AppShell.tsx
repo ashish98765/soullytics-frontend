255a734cb470d70d5072e2479fe8bb030f8a8dc0
ashish98765 <32891451+ashish98765@users.noreply.github.com>
2026-01-11 14:50:39 +0530

    Update AppShell.tsx

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,21 +1,7 @@
-import Sidebar from "./Sidebar";
-import Topbar from "./Topbar";
-
-export default function AppShell({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-
-      <div className="flex-1 flex flex-col">
-        <Topbar />
-        <main className="flex-1 p-6 bg-zinc-950">
-          {children}
-        </main>
-      </div>
+    <div style={{ background: "#000", color: "#fff", minHeight: "100vh", padding: 20 }}>
+      {children}
     </div>
   );
 }
